96c41fb024e8992a06ac1a80790d53bb37365ed0
JulRiv07 <Julian.rivera@utp.edu.co>
2026-03-12 14:56:54 -0500

    Parcial terminado ahora si

--- Parcial1.py
+++ Parcial1.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+#Julian Rivera Sanchez y Juan Manuel Rojas Muñoz
 
 class Empleados:
     def __init__(self, nombre, ID, salario, años_experiencia):
@@ -16,56 +17,113 @@ class Empleados:
         
     def __str__(self):
         return f"Nombre: {self.nombre}, ID: {self.ID}, Salario: {self.salario}, Años de experiencia: {self.años_experiencia}"  
-    
-    class GestorEmpleados:
-        def __init__(self):
-            self.empleados = []
-        def agregar_empleado(self, empleado):
-            self.empleados.append(empleado)
-        def eliminar_empleado(self, ID):
-            for empleado in self.empleados:
-                if empleado.ID == ID:
-                    self.empleados.remove(empleado)
-                    return True
-            return False
-    
-        def buscar_empleado(self, ID):
-            for empleado in self.empleados:
-                if empleado.ID == ID:
-                    return empleado
-            return None
-        
-        def editar_empleado(self, ID, nombre=None, salario=None, años_experiencia=None):
-            empleado = self.buscar_empleado(ID)
-            if empleado:
-                if nombre:
-                    empleado.nombre = nombre
-                if salario:
-                    empleado.salario = salario
-                if años_experiencia:
-                    empleado.años_experiencia = años_experiencia
+class GestorEmpleados:
+    def __init__(self):
+        self.empleados = []
+    def agregar_empleado(self, empleado):
+        self.empleados.append(empleado)
+    def eliminar_empleado(self, ID):
+        for empleado in self.empleados:
+            if empleado.ID == ID:
+                self.empleados.remove(empleado)
                 return True
-            return False
-        
-        def mostrar_empleados(self):
+        return False
+
+    def buscar_empleado(self, ID):
+        for empleado in self.empleados:
+            if empleado.ID == ID:
+                return empleado
+        return None
+    
+    def editar_empleado(self, ID, nombre=None, salario=None, años_experiencia=None):
+        empleado = self.buscar_empleado(ID)
+        if empleado:
+            if nombre:
+                empleado.nombre = nombre
+            if salario:
+                empleado.salario = salario
+            if años_experiencia:
+                empleado.años_experiencia = años_experiencia
+            return True
+        return False
+    
+    def mostrar_empleados(self):
+        for empleado in self.empleados:
+            print(empleado)
+    def guardar_empleados(self):
+        with open("empleados.txt", "w") as file:
             for empleado in self.empleados:
+                file.write(f"{empleado.nombre},{empleado.ID},{empleado.salario},{empleado.años_experiencia}\n")
+    
+    def cargar_empleados(self):
+        try:
+            with open("empleados.txt", "r") as file:
+                for line in file:
+                    nombre, ID, salario, años_experiencia = line.strip().split(",")
+                    empleado = Empleados(nombre, ID, float(salario), int(años_experiencia))
+                    self.agregar_empleado(empleado)
+        except FileNotFoundError:
+            print("No se encontró el archivo.")
+
+def menu_empleados():
+    gestor = Empleados.GestorEmpleados()
+    gestor.cargar_empleados()
+
+    while True:
+        print("\nSistema de Gestión de Empleados")
+        print("1. Agregar empleado")
+        print("2. Eliminar empleado")
+        print("3. Buscar empleado por ID")
+        print("4. Mostrar empleados")
+        print("5. Guardar empleados")
+        print("6. Salir")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            nombre = input("Nombre: ")
+            ID = input("ID: ")
+            salario = float(input("Salario: "))
+            años = int(input("Años de experiencia: "))
+
+            empleado = Empleados(nombre, ID, salario, años)
+            gestor.agregar_empleado(empleado)
+            print("Empleado agregado correctamente.")
+
+        elif opcion == "2":
+            ID = input("Ingrese el ID del empleado a eliminar: ")
+            if gestor.eliminar_empleado(ID):
+                print("Empleado eliminado.")
+            else:
+                print("Empleado no encontrado.")
+
+        elif opcion == "3":
+            ID = input("Ingrese el ID a buscar: ")
+            empleado = gestor.buscar_empleado(ID)
+
+            if empleado:
+                print("\nEmpleado encontrado:")
                 print(empleado)
+                print("Salario con aumento:", empleado.calcular_salario())
+            else:
+                print("Empleado no encontrado.")
 
-        def guardar_empleados(self):
-            with open("empleados.txt", "w") as file:
-                for empleado in self.empleados:
-                    file.write(f"{empleado.nombre},{empleado.ID},{empleado.salario},{empleado.años_experiencia}\n")
-        
-        def cargar_empleados(self):
-            try:
-                with open("empleados.txt", "r") as file:
-                    for line in file:
-                        nombre, ID, salario, años_experiencia = line.strip().split(",")
-                        empleado = Empleados(nombre, ID, float(salario), int(años_experiencia))
-                        self.agregar_empleado(empleado)
-            except FileNotFoundError:
-                print("No se encontró el archivo.")
+        elif opcion == "4":
+            print("\nLista de empleados:")
+            gestor.mostrar_empleados()
 
+        elif opcion == "5":
+            gestor.guardar_empleados()
+            print("Empleados guardados en archivo.")
+
+        elif opcion == "6":
+            gestor.guardar_empleados()
+            print("Saliendo del sistema...")
+            main()
+            break
+
+        else:
+            print("Opción inválida.")
 class Producto:
 
     def __init__ (self, nombre: str, id: int, precio: float, cantidadEnInventario: int):
@@ -197,100 +255,6 @@ class Tienda:
         except Exception as e:
             print(f"Ocurrió un error al cargar los datos: {e}")
 
-class Cesar:
-    def __init__(self, k = 0):
-        self.k = k
-        self.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-        self.alphabetDisplaced = self.alphabet[-self.k:] + self.alphabet[:-self.k]
-
-    def __str__(self):
-        return f"{self.alphabet}\n k = {self.k}\n{self.alphabetDisplaced}"
-    
-    def modifyK(self, k):
-        self.k = k
-        self.alphabetDisplaced = self.alphabet[-self.k:] + self.alphabet[:-self.k]
-
-    def encode(self, text):
-        encryptedText = ""
-        for i in range(len(text)):
-            if text[i] != ' ':
-                encryptedText += self.alphabetDisplaced[self.alphabet.index(text[i])]
-            else:
-                encryptedText += ' '
-
-        return encryptedText
-    
-    def deEncode(self, encryptedText):
-        text = ""
-        for i in range(len(encryptedText)):
-            if encryptedText[i] != ' ':
-                text += self.alphabet[self.alphabetDisplaced.index(encryptedText[i])]
-            else:
-                text += ' '
-
-        return text
-
-
-def menu_empleados():
-    gestor = Empleados.GestorEmpleados()
-    gestor.cargar_empleados()
-
-    while True:
-        print("\nSistema de Gestión de Empleados")
-        print("1. Agregar empleado")
-        print("2. Eliminar empleado")
-        print("3. Buscar empleado por ID")
-        print("4. Mostrar empleados")
-        print("5. Guardar empleados")
-        print("6. Salir")
-
-        opcion = input("Seleccione una opción: ")
-
-        if opcion == "1":
-            nombre = input("Nombre: ")
-            ID = input("ID: ")
-            salario = float(input("Salario: "))
-            años = int(input("Años de experiencia: "))
-
-            empleado = Empleados(nombre, ID, salario, años)
-            gestor.agregar_empleado(empleado)
-            print("Empleado agregado correctamente.")
-
-        elif opcion == "2":
-            ID = input("Ingrese el ID del empleado a eliminar: ")
-            if gestor.eliminar_empleado(ID):
-                print("Empleado eliminado.")
-            else:
-                print("Empleado no encontrado.")
-
-        elif opcion == "3":
-            ID = input("Ingrese el ID a buscar: ")
-            empleado = gestor.buscar_empleado(ID)
-
-            if empleado:
-                print("\nEmpleado encontrado:")
-                print(empleado)
-                print("Salario con aumento:", empleado.calcular_salario())
-            else:
-                print("Empleado no encontrado.")
-
-        elif opcion == "4":
-            print("\nLista de empleados:")
-            gestor.mostrar_empleados()
-
-        elif opcion == "5":
-            gestor.guardar_empleados()
-            print("Empleados guardados en archivo.")
-
-        elif opcion == "6":
-            gestor.guardar_empleados()
-            print("Saliendo del sistema...")
-            main()
-            break
-
-        else:
-            print("Opción inválida.")
-
 def menu_tienda():
     mi_tienda = Tienda()
 
@@ -354,31 +318,43 @@ def menu_tienda():
             break
         else:
             print("Opción no válida.")
+class Cesar:
+    def __init__(self, k = 0):
+        self.k = k
+        self.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        self.alphabetDisplaced = self.alphabet[-self.k:] + self.alphabet[:-self.k]
 
-def ejercicio4():
-    print("Punto 4 del parcial I\n")
-    num = (int(input("Ingrese un numero de 4 cifras: ")))
+    def __str__(self):
+        return f"{self.alphabet}\n k = {self.k}\n{self.alphabetDisplaced}"
+    
+    def modifyK(self, k):
+        self.k = k
+        self.alphabetDisplaced = self.alphabet[-self.k:] + self.alphabet[:-self.k]
 
-    firstDigit = num // 1000
-    secondDigit = (num % 1000) // 100
-    thirdDigit = (num % 100) // 10
-    fourthDigit = num % 10
+    def encode(self, text):
+        encryptedText = ""
+        for i in range(len(text)):
+            if text[i] != ' ':
+                encryptedText += self.alphabetDisplaced[self.alphabet.index(text[i])]
+            else:
+                encryptedText += ' '
 
-    if firstDigit % fourthDigit == 0:
-        print(f"{firstDigit} SI es multiplo de {fourthDigit}")
-    else:
-        print(f"{firstDigit} NO es multiplo de {fourthDigit}")
+        return encryptedText
+    
+    def deEncode(self, encryptedText):
+        text = ""
+        for i in range(len(encryptedText)):
+            if encryptedText[i] != ' ':
+                text += self.alphabet[self.alphabetDisplaced.index(encryptedText[i])]
+            else:
+                text += ' '
 
-    sumSecondThird = secondDigit + thirdDigit
-    print (f"La suma del secondDigit y el tercer numero es: {sumSecondThird}")
+        return text
 
 def julio_cesar():
-    print("Este es un codigo que con una clase 'Cesar' que recibe un desplazamiento 'k' y usando sus metodos se puede cifrar o decifrar una palabra con la 'k' dada.")
-    
+
     encription = Cesar(0)
     while 1:
-        # print(encription)
-
         print("\n--- MENÚ ---")
         print("1. Ingresar desplazamiento")
         print("2. Encriptar una frase")
@@ -403,6 +379,22 @@ def julio_cesar():
             break
         else:
             print("Opción no valida, intente de nuevo.")
+
+def ejercicio4():
+    num = (int(input("Ingrese un numero de 4 cifras: ")))
+
+    firstDigit = num // 1000
+    secondDigit = (num % 1000) // 100
+    thirdDigit = (num % 100) // 10
+    fourthDigit = num % 10
+
+    if firstDigit % fourthDigit == 0:
+        print(f"{firstDigit} SI es multiplo de {fourthDigit}")
+    else:
+        print(f"{firstDigit} NO es multiplo de {fourthDigit}")
+
+    sumSecondThird = secondDigit + thirdDigit
+    print (f"La suma del secondDigit y el tercer numero es: {sumSecondThird}")
 
 def main():
     opc = int(input("\nIngrese el numero del ejercicio a ejecutar (2, 3, 4 o 5) o 1 para salir: "))
